162500f442a29caa1a963514f20c4e0f10d116f3
Janet Zheng <janet.zhengg@gmail.com>
2020-06-08 11:51:26 -0700

    more notes added, need constants declared

--- app.js
+++ app.js
@@ -2,13 +2,36 @@
   create a variable that adds a X class
   create a variable that adds a O class
   declare winning conditions
+  declare boolen for checking turn 
 
-  listen for a click and only listen for one click <- apply to call the cells
+  need initialize game function
+    listen for a click and only listen for one click <- apply to call the cells
+    set turn to X
 
   need to handle click
+    place marker down
+    check win
+    check draw
+    switch turns
+
   need to place a marker down
-    need a class X or O and put text down
+    check for who's turn
+      need a class X or O and put text down
+  
   need to switch turns
   need to check win
   need to check for draw
 */
+
+const xClass = 'X';
+const oClass = 'O';
+const winningCombos = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [1, 4, 7],
+  [2, 5, 8],
+  [3, 6, 9],
+  [1, 5, 9],
+  [3, 5, 7]
+];
