6d36787aae0202042c2e494fa9a139d45d03cdc1
Janet Zheng <janet.zhengg@gmail.com>
2020-06-08 11:41:01 -0700

    some notes added

---
+++ app.js
@@ -0,0 +1,14 @@
+/*
+  create a variable that adds a X class
+  create a variable that adds a O class
+  declare winning conditions
+
+  listen for a click and only listen for one click <- apply to call the cells
+
+  need to handle click
+  need to place a marker down
+    need a class X or O and put text down
+  need to switch turns
+  need to check win
+  need to check for draw
+*/
